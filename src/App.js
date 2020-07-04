7f7e99091362dc68057e345bad3bce24a191be40
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2020-07-04 19:54:21 +0530

    Removing unused react files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import './App.css';
-import Login from './Components/Login';
-import Countdown from './Components/Countdown';
-import darkTheme from './Themes/DarkTheme';
-import { MuiThemeProvider } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MuiThemeProvider } from '@material-ui/core';
+import darkTheme from './Themes/DarkTheme';
+import Countdown from './Components/Countdown';
+import Login from './Components/Login';
 
 function App() {
   return (
     <MuiThemeProvider theme={darkTheme}>
-      <div className="App">
         <Router>
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/play" component={Countdown} />
           </Switch>
         </Router>
-      </div>
     </MuiThemeProvider>
   );
 }

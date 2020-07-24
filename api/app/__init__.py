e90db6c560cf5aa26d1b65ea8ead5961f4e0ee34
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2020-07-24 20:56:44 +0530

    Removing unused import and adding requirements file

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,6 @@ import logging
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 from flask_socketio import SocketIO
-from flask_session import Session
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
